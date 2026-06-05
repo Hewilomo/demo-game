e78faa057db39f7d96cbfb8158db6c0a61066521
ealesaph <enrique.alegria02@inacapmail.cl>
2026-06-04 20:55:24 -0400

    baby first steps

---
+++ class/Personaje/Personaje.py
@@ -0,0 +1,24 @@
+# Base de creación de clases
+# Atributos
+class Personaje:
+    def __init__(self, nombre='', dinero=5000, vida=100, vivo=True):
+        self.nombre = nombre
+        self.dinero = dinero
+        self.vida = vida
+        self.vivo = vivo
+        
+# Getter y Setters
+
+# Metodos
+def crearPersonaje(self, nombre):
+    nombrePersonaje = nombre
+    pass
+
+def intercambiarDineroPorVida(self):
+    pass
+
+def moverPersonaje(self):
+    pass
+
+def morirPersonaje(self):
+    pass
---
+++ config/Config.py
@@ -0,0 +1,11 @@
+class Config:
+    
+    def __init__(self):
+        pass
+    
+# Getter y Setter
+
+# Metodos
+def guardar(self):
+    # Guardar datos en Json
+    pass
